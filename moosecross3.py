0da0e496c267d6d71de2426e764f73533f566d5b
Andy Elvey <andy@obsidian.(none)>
2009-06-06 22:33:46 +1200

    More code. Program now reads file into a dictionary.

--- moosecross3.py
+++ moosecross3.py
@@ -5,8 +5,9 @@
 #  This code is released to the public domain.  
 #  "Share and enjoy....."  :) 
 
-#  Note = as at now (4th June), this code is not working, but 
-#  it should be before much longer.  
+#  Status - as at now (6th June), this code reads a file 
+#  into a dictionary.  It shouldn't be long before the actual 
+#  crosstab code is added and working.   
 
 #  It is eventually planned to add "traffic-lighting" and 
 #  the ability to output the crosstab to HTML and text files. 
@@ -17,43 +18,35 @@ import itertools
 class crosstab(): 
    def init(self): 
       self.rowvar = self.colvar = self.sumvar = None 
-	  self.filedict = {} 
-	  self.headingslist = []
-	  self.linelist = [] 
-	  self.colslist = [] 
+      self.filedict = {} 
+      self.headingslist = []
+      self.linelist = [] 
+      self.colslist = [] 
 	  
 	  	  
-   def read(self, file, sep=","): 
-      self.myfile = open(file, 'r')  
-	  for line in self.myfile.readlines(): 
-	      self.datalist.append(list(line.rstrip('\n').split(',') ))  
-      self.colslist = zip(*self.datalist) 	   		  
-	  self.headingslist = self.colslist[0]  
+   def read(self, file, sep=","):       
       ''' Open the file ''' 
-	  f = open('testdata.csv', 'r').readlines() 
-	  ''' Convert it into lists of lines ''' 
-	  for line in f: 
+      self.file = open(file, 'r').readlines() 
+      ''' Convert it into lists of lines ''' 
+      for line in self.file:   
           self.linelist.append(list(line.rstrip('\n').split(',') ))
       ''' Convert it into a list of columns ''' 
-	  self.colslist = zip(*self.lineslist) 		  
-	  ''' Read it into a dictionary ''' 
-	  for x in range(0, len(self.colslist) ):
+      self.colslist = zip(*self.linelist) 		  
+      ''' Read it into a dictionary ''' 
+      for x in range(0, len(self.colslist) ):
           self.filedict.update({ self.colslist[x][0]: self.colslist[x][1:]})  
 	  
       
    def crosstab(self, rowvar, colvar, sumvar): 
-      
+      ''' 
       if self.filedict.has_key(rowvar) and 	\ 
 	     self.filedict.has_key(colvar) and  \  
 		 self.filedict.has_key(sumvar): 
-		 product = itertool.product(rowvar, colvar) 
-		 prodsum = reduce.... ''' to be finished ... ''' 
-		     
-			 
-			 
+		    product = itertool.product(rowvar, colvar) 
+		    prodsum = reduce....  to be finished ... '''  
+		     						
    def display(self): 
-      print self.prodsum  
-
+       print self.filedict 
 
 	  
 #  Run the code 
@@ -63,7 +56,7 @@ a.init()
 
 a.read('testdata.csv', sep=",")  
 
-a.crosstab("city", "salesman", "tot_sales") 	  
+#  a.crosstab("city", "salesman", "tot_sales") 	  
 
 a.display() 
 
